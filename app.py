01b66f86270cd9feb1f652c2b2757f371854ded4
habibulloxon <hxayrulloxojayev@webster.edu>
2024-12-09 23:35:31 +0500

    [feat]: filters added & UI improved

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 import os
 
@@ -15,10 +15,15 @@ cursor = conn.cursor()
 
 @app.route('/')
 def index():
-    level = "Graduate"
+    level = request.args.get('level', 'Undergraduate') 
+
     cursor.execute("SELECT course_name, day_of_week, start_time, end_time FROM courses WHERE level = %s", (level,))
     courses = cursor.fetchall()
-    return render_template('index.html', courses=courses, student_name="Habibulloxon Xayrulloxo'jayev")
+
+    cursor.execute("SELECT DISTINCT level FROM courses")
+    levels = [row[0] for row in cursor.fetchall()]
+    
+    return render_template('index.html', courses=courses, levels=levels, selected_level=level, student_name="Habibulloxon Xayrulloxo'jayev")
 
 if __name__ == '__main__':
     app.run(debug=True)
